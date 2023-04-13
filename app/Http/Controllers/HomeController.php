482e897ec3d4c6ca4b0a94d1c8cc96f22aeecfaf
hossamghoneim <hossamghoneim11@gmail.com>
2023-04-13 02:12:58 +0200

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('dashboard.teams.index');
+        $teams = Team::query()->paginate(10);
+
+        return view('dashboard.teams.index', compact('teams'));
     }
 
 
